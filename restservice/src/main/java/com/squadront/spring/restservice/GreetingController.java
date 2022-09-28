8324cb5aace544770546dcb5af845ea413798617
Yoga Hapriana <thenelse.v2@gmail.com>
2022-09-28 18:44:37 +0700

    Create a Resource Controller

---
+++ restservice/src/main/java/com/squadront/spring/restservice/GreetingController.java
@@ -0,0 +1,21 @@
+package com.squadront.spring.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yogahapriana on 28/09/2022 - 18.41
+ */
+@RestController
+public class GreetingController {
+
+  private static final String template = "Hello %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @GetMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  }
+}
