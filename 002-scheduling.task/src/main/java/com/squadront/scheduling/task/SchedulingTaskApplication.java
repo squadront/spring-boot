50aa3ee1f10aff7ff35ecb627746528d48b4fb48
Yoga Hapriana <thenelse.v2@gmail.com>
2022-09-28 19:37:14 +0700

    Starting with Spring Initializr

---
+++ 002-scheduling.task/src/main/java/com/squadront/scheduling/task/SchedulingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.squadront.scheduling.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTaskApplication.class, args);
+	}
+
+}
---
+++ 002-scheduling.task/src/test/java/com/squadront/scheduling/task/SchedulingTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadront.scheduling.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
