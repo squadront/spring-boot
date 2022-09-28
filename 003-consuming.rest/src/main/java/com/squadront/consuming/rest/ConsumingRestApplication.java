092b1f341b7bee7d6617f1b56b3794f678ef0867
Yoga Hapriana <thenelse.v2@gmail.com>
2022-09-28 20:53:52 +0700

    Starting with Spring Initializr

---
+++ 003-consuming.rest/src/main/java/com/squadront/consuming/rest/ConsumingRestApplication.java
@@ -0,0 +1,13 @@
+package com.squadront.consuming.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+
+}
