29469556ac02937f817611ecc7d37dcf74673740
Yoga Hapriana <thenelse.v2@gmail.com>
2022-09-28 21:07:56 +0700

    Feature Spring Boot Consuming Rest (#3)
    * Starting with Spring Initializr
    * Fetching a REST Resource
    * Finishing the Application

---
+++ 003-consuming.rest/src/main/java/com/squadront/consuming/rest/ConsumingRestApplication.java
@@ -0,0 +1,32 @@
+package com.squadront.consuming.rest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@Slf4j
+public class ConsumingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			User user = restTemplate.getForObject("https://random-data-api.com/api/v2/users", User.class);
+			assert user != null;
+			log.info(user.toString());
+		};
+	}
+}
